956a945b2d335b399207c43f539404f1038b5712
Gaurav-kedia <gaurav.kedia18@gmail.com>
2020-05-03 08:25:05 +0530

    login, logout done

---
+++ app/src/main/java/com/gaurav/pnc/Home_activity.java
@@ -0,0 +1,111 @@
+package com.gaurav.pnc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Home_activity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle mtoggle;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        mAuth = FirebaseAuth.getInstance();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+
+        //mtoggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(mtoggle);
+        //mtoggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.item1:
+                        return true;
+
+                    case R.id.item2:
+                        return true;
+
+                    case R.id.item3:
+                        return true;
+
+                    case R.id.item4:
+                        return true;
+
+                    case R.id.item5:
+                        return true;
+
+                }
+                return true;
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            SendUserToLoginActivity();
+        }
+        else{
+           /* updateuserstatus("online");
+            verifyuserexistance();*/
+        }
+        super.onStart();
+    }
+
+    private void SendUserToLoginActivity() {
+        startActivity(new Intent(Home_activity.this, login_activity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.logout_option:
+                mAuth.signOut();
+                SendUserToLoginActivity();
+                finish();
+                return true;
+        }
+        /*if(mtoggle.onOptionsItemSelected(item)){
+            return true;
+        }*/
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}

e959ba00a16b473fe5731d9a2772869444d05cda
balgopal123 <n.balgopalpatra@gmail.com>
2020-05-08 20:06:28 +0530

    Added Images to cards

--- app/src/main/java/com/gaurav/pnc/Home_activity.java
+++ app/src/main/java/com/gaurav/pnc/Home_activity.java
@@ -1,6 +1,8 @@
 package com.gaurav.pnc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -10,11 +12,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -103,6 +108,15 @@ public class Home_activity extends AppCompatActivity {
         });
 
     }
+
+    public void gotoCourse(View view){
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        Intent i = new Intent(Home_activity.this,Courses.class);
+        i.putExtra("Course",buttonText.toUpperCase());
+        startActivity(i);
+    }
+
     @Override
     protected void onStart() {
         FirebaseUser currentUser = mAuth.getCurrentUser();

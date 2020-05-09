47ded25724553237dd0cf3cc9e5d15e896bd6922
Gaurav-kedia <gaurav.kedia18@gmail.com>
2020-05-10 02:47:06 +0530

    updated home pade, nav view

--- app/src/main/java/com/gaurav/pnc/Profile_Activity.java
+++ app/src/main/java/com/gaurav/pnc/Profile_Activity.java
@@ -1,5 +1,6 @@
 package com.gaurav.pnc;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile_Activity extends AppCompatActivity {
 
@@ -41,6 +45,19 @@ public class Profile_Activity extends AppCompatActivity {
     }
 
     private void retrieveuserinfo() {
+        rootref.child("Users").child(currentuserid).child("name")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String user_name = dataSnapshot.getValue().toString().trim();
+                        name.setText(user_name);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     private void initializefields() {
